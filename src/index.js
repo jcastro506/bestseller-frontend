a5ea86b3044a6dd8ebcd84d5fd0d5509fb629744
nohaderf <freda.hon@gmail.com>
2021-01-04 18:15:37 -0500

    basic layout

---
+++ src/index.js
@@ -0,0 +1,30 @@
+/* DOM ELEMENTS */
+const mainContainer = document.querySelector("#book-collection")
+// const bookImg = document.querySelector(".book-img")
+// const bookTitle = document.querySelector("#title")
+// const bookAuthor = document.querySelector("#author")
+
+
+/* Render Functions */
+function renderBooks (bookObj) {
+    console.log(bookObj)
+    const divTile = document.createElement("div")
+    divTile.className = "tile"
+    divTile.dataset.id = bookObj.id
+    divTile.innerHTML += `
+        <img class="book-img" src="${bookObj.image_url}" alt="${bookObj.title}"/>
+        <h2>${bookObj.title}</h2>
+        <h3><em>${bookObj.author}</em></h3>
+        <a href="">Review</a>
+    `
+
+    mainContainer.append(divTile)
+}
+
+
+/* Fetch */
+fetch(`http://localhost:3000/books/`)
+ .then(r => r.json())
+ .then(bookArray => bookArray.forEach(renderBooks))
+
+/* Initialize */
